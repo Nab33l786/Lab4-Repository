9050c2b6977b35bffaab0950de687413ce19215b
Nabeel Ahmed <nabeelah@umich.edu>
2019-01-31 17:52:39 -0500

    Added eat() method to Bob branch

--- Duck.java
+++ Duck.java
@@ -2,6 +2,9 @@ Public class duck {
 	/* * Master branch is responsible for fly() method. */
 	public fly(){
 		system.out.println("A duck can fly");
+
+	}  
+
 	}  
 	/* * Tom branch is responsible for swim() method. */
 	public swim(){
