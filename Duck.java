ace8d40a30166e3b9277d3b6fbb12c21e961cf75
Nabeel Ahmed <nabeelah@umich.edu>
2019-01-31 19:24:49 -0500

    Added swim() method

--- Duck.java
+++ Duck.java
@@ -1,10 +1,7 @@
-Public class duck {
-	/* * Master branch is responsible for fly() method. */
-	public fly(){
-		system.out.println("A duck can fly");
-	}  
+
  
 	/* * Tom branch is responsible for swim() method. */
 	public swim(){
 		system.out.println("A duck can swim");
 	}
+	
